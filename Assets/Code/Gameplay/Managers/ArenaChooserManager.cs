2903242627b731f914700b42b24ca06aa7b1384b
Sebastian Blaszczyk <skb3@op.pl>
2024-04-23 20:07:39 +0200

    Added empty map managers and time manager

---
+++ Assets/Code/Gameplay/Managers/ArenaChooserManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay.Arena
+{
+    public class ArenaChooserManager : MonoSingleton<ArenaChooserManager>
+    {
+        #region VARIABLES
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region METHODS
+
+        #endregion
+    }
+}
