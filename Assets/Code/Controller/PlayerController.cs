e5b2b15688ff7625abd4a6bcef0d199755560e07
Sebastian Blaszczyk <skb3@op.pl>
2024-07-21 18:44:04 +0200

    Added base of player controller

---
+++ Assets/Code/Controller/PlayerController.cs
@@ -0,0 +1,112 @@
+using GlobalEventSystem;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace Gameplay.Character
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerController : MonoBehaviour
+    {
+        #region VARIABLES
+
+        [SerializeField, FoldoutGroup("Values")] float walkSpeed;
+        [SerializeField, FoldoutGroup("Values")] float runSpeed;
+        [SerializeField, FoldoutGroup("Values")] float jumpPower;
+        [SerializeField, FoldoutGroup("Values")] float gravity;
+
+        [SerializeField, FoldoutGroup("Components")] private Animator animator;
+        [SerializeField, FoldoutGroup("Components")] private Rigidbody rb;
+        [SerializeField, FoldoutGroup("Components")] private CapsuleCollider capsuleCollider;
+
+        private bool isSprinting;
+        private float baseDrag;
+
+        private Vector2 direction = Vector3.zero;
+        private Vector3 moveDirection = Vector3.zero;
+        private Vector3 velocity = Vector3.zero;
+
+        #endregion
+
+        #region PROPERTIES
+
+        private bool IsGrounded => Physics.Raycast(transform.position, Vector3.down, capsuleCollider.height * 0.5f + 0.2f);
+
+        #endregion
+
+        #region UNITY_METHODS
+
+        private void Start()
+        {
+            AttachEvents();
+            baseDrag = rb.drag;
+        }
+
+        private void OnDestroy()
+        {
+            DetachEvents();
+        }
+
+        private void Update()
+        {
+            MovePlayer();
+        }
+
+        #endregion
+
+        #region METHODS
+
+        void MovePlayer()
+        {
+            if (direction != Vector2.zero)
+            {
+                moveDirection = transform.right * direction.x + transform.forward * direction.y;
+                rb.AddForce(moveDirection * (isSprinting ? runSpeed : walkSpeed), ForceMode.Force);
+            }
+
+            rb.drag = IsGrounded ? baseDrag : 0;
+            Debug.Log(IsGrounded);
+            //    velocity.y -= gravity * Time.deltaTime;
+            //characterController.Move(velocity * Time.deltaTime);
+        }
+
+        private void AttachEvents()
+        {
+            Events.Gameplay.Move.OnJump += Jump;
+            Events.Gameplay.Move.OnMoveInDirection += MoveInDirection;
+            Events.Gameplay.Move.OnSprint += Sprint;
+        }
+
+        private void DetachEvents()
+        {
+            Events.Gameplay.Move.OnJump -= Jump;
+            Events.Gameplay.Move.OnMoveInDirection -= MoveInDirection;
+            Events.Gameplay.Move.OnSprint -= Sprint;
+        }
+
+        private void Jump()
+        {
+            //if (!IsGrounded)
+            //    return;
+
+            rb.AddForce(transform.up * jumpPower, ForceMode.Impulse);
+
+        }
+
+        private void MoveInDirection(Vector2 direction)
+        {
+            this.direction = direction;
+        }
+
+        private void Sprint(bool isSprinting)
+        {
+            this.isSprinting = isSprinting;
+        }
+
+        #endregion
+    }
+}
+
+
