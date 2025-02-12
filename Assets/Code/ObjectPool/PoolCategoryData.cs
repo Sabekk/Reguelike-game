2782dc0f613cbd2fecf80d5fca0f7c34c7416c91
Sebastian Blaszczyk <skb3@op.pl>
2025-02-12 13:29:46 +0100

    Added groups in object pool categories

--- Assets/Code/ObjectPool/PoolCategoryData.cs
+++ Assets/Code/ObjectPool/PoolCategoryData.cs
@@ -13,7 +13,7 @@ namespace ObjectPooling
 
         [SerializeField, ReadOnly] private int id = Guid.NewGuid().GetHashCode();
         [SerializeField] private string categoryName;
-        [SerializeField] private List<PoolInstanceData> instances;
+        [SerializeField] private List<PoolInstancesGroupData> groups;
 
         #endregion
 
@@ -21,7 +21,18 @@ namespace ObjectPooling
 
         public int Id => id;
         public string CategoryName => categoryName;
-        public List<PoolInstanceData> Instances => instances;
+        public List<PoolInstanceData> Instances
+        {
+            get
+            {
+                List<PoolInstanceData> instances = new();
+                for (int i = 0; i < groups.Count; i++)
+                    instances.AddRange(groups[i].Instances);
+
+                return instances;
+            }
+        }
+        public List<PoolInstancesGroupData> Groups => groups;
 
 
         #endregion
@@ -40,34 +51,78 @@ namespace ObjectPooling
 
         public void AddInstance(PoolInstanceData instance)
         {
-            instances.Add(instance);
+            //instances.Add(instance);
+
+            PoolInstancesGroupData group = FindGroup(instance.Name);
+            if (group == null)
+            {
+                group = new(GetGroupName(instance.Name));
+                Groups.Add(group);
+            }
+
+            group.AddInstance(instance);
         }
 
         public void RemoveInstance(PoolInstanceData instance)
         {
-            instances.Remove(instance);
+            //instances.Remove(instance);
+
+            PoolInstancesGroupData group = FindGroup(instance.Name);
+            if (group == null)
+                return;
+
+            group.RemoveInstance(instance);
+            if (group.Instances.Count <= 0)
+                Groups.Remove(group);
         }
 
         public PoolInstanceData FindInstanceData(string instanceDataName)
         {
-            foreach (var instanceData in Instances)
-            {
-                if (instanceData.Name == instanceDataName)
-                    return instanceData;
-            }
-
+            PoolInstancesGroupData group = FindGroup(GetGroupName(instanceDataName));
+            if (group != null)
+                return group.FindInstanceData(instanceDataName);
             return null;
         }
 
         public PoolInstanceData FindInstanceData(int instanceDataId)
         {
-            foreach (var instanceData in Instances)
+            PoolInstancesGroupData group = FindGroup(instanceDataId, out PoolInstanceData instanceData);
+            if (group != null)
+                return instanceData;
+
+            return null;
+        }
+
+        public PoolInstancesGroupData FindGroup(string instanceDataName)
+        {
+            string groupName = GetGroupName(instanceDataName);
+            return Groups.Find(x => x.GroupName == groupName);
+        }
+
+        public PoolInstancesGroupData FindGroup(int instanceDataId, out PoolInstanceData instanceData)
+        {
+            instanceData = null;
+
+            foreach (var group in Groups)
             {
-                if (instanceData.IdEquals(instanceDataId))
-                    return instanceData;
+                instanceData = group.FindInstanceData(instanceDataId);
+                if (instanceData != null)
+                    return group;
             }
 
             return null;
+        }
+
+        public string GetGroupName(string instanceDataName)
+        {
+            int lastIndexOfNumber = instanceDataName.LastIndexOf("_");
+            if (lastIndexOfNumber < 0)
+                return instanceDataName;
+            else
+            {
+                string groupName = instanceDataName.Remove(lastIndexOfNumber, instanceDataName.Length - lastIndexOfNumber);
+                return groupName;
+            }
         }
 
         public bool IdEquals(int id)
